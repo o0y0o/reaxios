1386e13bba0545cfe4f1332272bd65f71f0d8c4c
Jason Chung <shiningjason1989@gmail.com>
2021-03-01 10:56:57 +0800

    update 3rd party libraries (#71)

---
+++ .babelrc.js
@@ -0,0 +1,1 @@
+module.exports = require('@0y0/babel-preset-vanilla')(undefined, {})
--- rollup.config.js
+++ rollup.config.js
@@ -8,39 +8,40 @@ const external = [pkg.dependencies, pkg.peerDependencies]
   .flatMap(dep => Object.keys(dep))
   .map(pkg => new RegExp(`^${pkg.replace(/\//g, '/')}`))
 
-export default [
-  {
-    input: path.join(__dirname, pkg.src),
-    external,
-    plugins: [babel({ exclude: 'node_modules/**', babelHelpers: 'runtime' })],
-    output: [
-      {
-        file: path.join(__dirname, pkg.main),
-        format: 'cjs',
-        exports: 'default'
-      },
-      {
-        file: path.join(__dirname, pkg.module),
-        format: 'es'
-      }
-    ]
-  },
-  {
-    input: path.join(__dirname, pkg.src),
-    external,
-    plugins: [
-      babel({
-        exclude: 'node_modules/**',
-        babelHelpers: 'bundled',
-        plugins: [['@babel/transform-runtime', false]]
-      })
-    ],
-    output: {
-      file: path.join(__dirname, pkg.iife),
-      format: 'iife',
-      name: 'Reaxios',
-      globals: { axios: true },
-      plugins: [terser()]
+const pkgConfig = {
+  input: path.join(__dirname, pkg.src),
+  external,
+  plugins: [babel({ exclude: 'node_modules/**', babelHelpers: 'runtime' })],
+  output: [
+    {
+      file: path.join(__dirname, pkg.main),
+      format: 'cjs',
+      exports: 'default'
+    },
+    {
+      file: path.join(__dirname, pkg.module),
+      format: 'es'
     }
+  ]
+}
+
+const iifeConfig = {
+  input: path.join(__dirname, pkg.src),
+  external,
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      babelHelpers: 'bundled',
+      plugins: [['@babel/transform-runtime', false]]
+    })
+  ],
+  output: {
+    file: path.join(__dirname, pkg.iife),
+    format: 'iife',
+    name: 'Reaxios',
+    globals: { axios: true },
+    plugins: [terser()]
   }
-]
+}
+
+export default [pkgConfig, iifeConfig]
