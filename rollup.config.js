b914b7eebc9fcb776f1d78b656df6166943e8054
jason.chung <shiningjason1989@gmail.com>
2021-06-24 05:36:11 +0800

    Upgrade packages and fix code scanning alerts

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,7 @@ import pkg from './package.json'
 const external = [pkg.dependencies, pkg.peerDependencies]
   .filter(Boolean)
   .flatMap(dep => Object.keys(dep))
-  .map(pkg => new RegExp(`^${pkg.replace(/\//g, '/')}`))
+  .map(pkg => new RegExp(`^${pkg}`))
 
 const pkgConfig = {
   input: path.join(__dirname, pkg.src),
